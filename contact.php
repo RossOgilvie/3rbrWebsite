ee7fa7fa8146fb340a6eab016e4860097daf6b22
Ross Ogilvie <ross@rossogilvie.id.au>
2012-11-17 03:07:40 +1100

    Tweak site banner

--- contact.php
+++ contact.php
@@ -7,47 +7,39 @@
 <?php include("site-header.html"); ?>
 <?php include("nav-menu.html"); ?>
 
-<div class="content">
+<div class="content" style="text-align: left;">
 <h2>Contact Us</h2>
 	
-<p>If you would like to join, or just have a question, please send us an email. 3rbrsecretary@gmail.com</p>
+<p>If you would like to join, or just have a question, please send us an email at 3rbrsecretary@gmail.com</p>
 	
 <h2>Crew Committee</h2>
 
 <p><strong>Crew Leader</strong> <br />
-<a href="profiles.php#Cecilia Jackson">Cecilia Jackson</a><br />
-Email: crewleader@rosebayrovers.org.au</p>
+<a href="profiles.php#Cecilia Jackson">Cecilia Jackson</a>: crewleader@rosebayrovers.org.au</p>
 
 <p><strong>Rover Advisor</strong> <br />
-<a href="profiles.php#Phil Wicks">Phil Wicks</a><br />
-Email: ra@rosebayrovers.org.au</p>
+<a href="profiles.php#Phil Wicks">Phil Wicks</a>: ra@rosebayrovers.org.au</p>
 
 <p><strong>Secretary</strong> <br />
-<a href="profiles.php#Ayla Jones">Ayla Jones</a><br />
-Email: 3rbrsecretary@gmail.com</p>
+<a href="profiles.php#Ayla Jones">Ayla Jones</a>: 3rbrsecretary@gmail.com</p>
 
 <p><strong>Treasurer</strong> <br />
-<a href="profiles.php#Ross Ogilvie">Ross Ogilvie</a><br />
-Email: treasurer@rosebayrovers.org.au</p>
+<a href="profiles.php#Ross Ogilvie">Ross Ogilvie</a>: treasurer@rosebayrovers.org.au</p>
 
 <p><strong>Quartermaster</strong> <br />
-<a href="profiles.php#Alice Anderson">Alice Anderson</a><br />
-Email: quartermaster@rosebayrovers.org.au</p>
+<a href="profiles.php#Alice Anderson">Alice Anderson</a>: quartermaster@rosebayrovers.org.au</p>
 
 <p><strong>Service Co-ordinator</strong> <br />
-<a href="profiles.php#James Wallace">James Wallace</a><br />
-Email: service@rosebayrovers.org.au</p>
+<a href="profiles.php#James Wallace">James Wallace</a>: service@rosebayrovers.org.au</p>
 
 <p><strong>Venturer Liaisons</strong> <br />
-<a href="profiles.php#Ross Ogilvie">Ross Ogilvie</a>, <a href="profiles.php#James Wallace">James Wallace</a>, <a href="profiles.php#Lachlan Frazer">Lachlan Frazer</a>, <a href="profiles.php#Alice Anderson">Alice Anderson</a><br />
-Email: venturer@rosebayrovers.org.au</p>
+<a href="profiles.php#Ross Ogilvie">Ross Ogilvie</a>, <a href="profiles.php#James Wallace">James Wallace</a>, <a href="profiles.php#Lachlan Frazer">Lachlan Frazer</a>, <a href="profiles.php#Alice Anderson">Alice Anderson</a>:<br /> venturer@rosebayrovers.org.au</p>
 
 <p><strong>RRC Representatives</strong> <br />
 <a href="profiles.php#Lachlan Frazer">Lachlan Frazer</a> and <a href="profiles.php#Tristan McGrath">Tristan McGrath</a></p>
 
 <p><strong>Webmaster</strong><br />
-<a href="profiles.php#Ross Ogilvie">Ross Ogilvie</a><br />
-Email: webmaster@rosebayrovers.org.au</p>
+<a href="profiles.php#Ross Ogilvie">Ross Ogilvie</a>: webmaster@rosebayrovers.org.au</p>
 </div>
 </body>
 </html>

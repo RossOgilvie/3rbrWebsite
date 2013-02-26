767399fa87780079a6d2f2885d9978f7fbd2b5ae
Ross Ogilvie <ross@rossogilvie.id.au>
2013-02-26 16:25:54 +1100

    tweaked about us

--- about.php
+++ about.php
@@ -9,9 +9,9 @@
 <div class="content">
 	<h2>About Us</h2>
 	
-	<p>3rd Rose Bay (Judean) Rover Crew is based in the eastern suburbs of Sydney. We are an open crew, usually meeting on Sunday evenings at 7pm (See our <a href="calendar">Calendar</a>). We are a social Crew who also get involved in adventurous activities and service work throughout the community, all whilst having a good time. Our activities have included Dragon Skin, Moots, hikes, movie nights, caving and other rock related activities.</p>
+	<p>3rd Rose Bay (Judean) Rover Crew is based in the eastern suburbs of Sydney. We are open to all men and women aged 18-26 years old, usually meeting on Sunday evenings at 7pm (See our <a href="calendar">Calendar</a>). Our Crew enjoys doing a mix of social and adventerous activites, as well as living out the Rover motto of "Service". Typical activities include Dragon Skin, Moots, hikes, movie nights, laser tag, caving and other rock related activities.</p>
 
-	<p>We have two scarfs which reflects our heritage as a crew. The 3rd Rose Bay (Judean) Scout Group is light blue with 2 white edging stripes. The Crew scarf is navy blue with light blue and white edging and is only worn by knights.</p>
+	<p>We have two scarfs that reflect our Crew's heritage. The 3rd Rose Bay (Judean) Scout Group scarf is light blue with two white edging stripes. The Crew scarf is the former scarf of 2nd North Bondi Scout Group. It is navy blue with light blue and white edging and is only worn by knights.</p>
 	
 	<p>Our Hall is located in Vickery Ave, Rose Bay </p>
 

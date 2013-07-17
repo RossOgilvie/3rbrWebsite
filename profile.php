9c948513bd81e153581430d983112a8bbee7e3f5
Ross Ogilvie <ross@rossogilvie.id.au>
2013-07-18 00:55:21 +1000

    was throwing soft 404s, which broke profile pic loading

--- profile.php
+++ profile.php
@@ -1,4 +1,3 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script type="text/javascript" src="js/marked.js"></script>
 <script type="text/javascript">
 //detect hash change and then 

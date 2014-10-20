68e57f854e8e04f5c15ca775af2c6bb93dd8e6d7
Ross Ogilvie <ross@rossogilvie.id.au>
2014-10-20 15:44:41 +1100

    docs is now a link to gdrive, which is 100x easier and nicer than some weird php or iframe embed.

--- docs.php
+++ docs.php
@@ -1,49 +1,6 @@
-<?php
-// Get the names of all rovers with a profile
-$groups=array();
-$docs=array();
-$dir0 = "documents/";  
-// Open a known directory, and proceed to read its contents  
-if (is_dir($dir0) && $dh0 = opendir($dir0))  
-{  
-	while (($dir1 = readdir($dh0)) !== false)  
-	{
-		//for all the nontrivial and nonhidden folders in it, loop through their files
-		if(is_dir($dir0."/".$dir1) && $dir1[0] != "." && $dh1 = opendir($dir0."/".$dir1))
-		{
-			//add the good folder to the list of groups
-			$groups[] = $dir1;
-			//prep an array to hold all the files belonging to this group
-			$docs[$dir1]=array();
-			while (($file = readdir($dh1)) !== false)  
-			{
-				//again filter the crap files
-				if($file[0] != ".")
-				{
-					//add the real files to the list of docs, indexed by their group
-					$docs[$dir1][] = $file;
-				}
-			}
-			closedir($dh1);
-			sort($docs[$dir1]);
-		}
-	}  
-	closedir($dh0);
-	sort($groups);
-}
-?>
-
 <div class="content">
-<?php
-foreach($groups as $group)
-{
-	echo "<h2>$group</h2>\n";
-	echo "<ul>\n";
-	foreach($docs[$group] as $doc)
-	{
-		echo "<li><a href='documents/$group/$doc'>$doc</a></li>\n";
-	}
-	echo "</ul>\n";
-}
-?>
+<div id="docs-container">
+<iframe src="https://drive.google.com/embeddedfolderview?id=0B6mV8Q7QU9EwQkZqU3h2UmNhWFk#list" frameborder="0"></iframe>
+</iframe>
+</div>
 </div>
